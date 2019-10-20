90228d2255b3a63f8431ec2a8ac30cd334752935
Edd321 <36410503+Edd321@users.noreply.github.com>
2019-10-20 22:02:10 +0200

    Add files via upload

---
+++ caesarCypherEncryptor.java
@@ -0,0 +1,24 @@
+package Algorithms;
+
+public class caesarCypherEncryptor {
+	
+	public static String caesarCypherEncryptorF (String str, int key) { 
+		StringBuilder newString = new StringBuilder();
+		for (int i = 0 ; i < str.length(); i ++) { 
+			int newCharInt = (int) str.charAt(i) + key;
+			while (newCharInt > 122) { 
+				newCharInt = newCharInt - (26); 
+			} 
+			newString.append((char) newCharInt);
+		}
+		
+		return newString.toString();
+	}
+	
+	public static void main (String[] args) { 
+		
+		System.out.println(caesarCypherEncryptorF("abc", 57));
+		
+	}
+
+}
