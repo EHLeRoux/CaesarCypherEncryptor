5c4a9da34f44e41ef7b05902bf3aff22a0207262
Edd321 <36410503+Edd321@users.noreply.github.com>
2019-10-25 19:02:10 +0200

    Update caesarCypherEncryptor.java

--- caesarCypherEncryptor.java
+++ caesarCypherEncryptor.java
@@ -3,6 +3,17 @@ package Algorithms;
 
 public class caesarCypherEncryptor {
 	
+	/*
+	 * This method takes in a string and a key of type int, it then proceeds 
+	 * to shift the string characters up in the alphabet by the set key amount. 
+	 * Sample input: ab, 2
+	 * Sample output: cd
+	 * The method also wraps around the alphabet for cases where the key shifts
+	 * the character pass the last letter of the alphabet. 
+	 * Sample input: ab, 26
+	 * Sample output: ab
+	 */
+	
 	public static String caesarCypherEncryptorF (String str, int key) { 
 		StringBuilder newString = new StringBuilder();
 		for (int i = 0 ; i < str.length(); i ++) { 
